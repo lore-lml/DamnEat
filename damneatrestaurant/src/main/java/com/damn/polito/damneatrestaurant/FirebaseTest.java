b8231de7e2bdb12b2c0e11abcb033a11e9f6fd9c
Stefano Loscalzo <stefano.loscalzo@gmail.com>
2019-04-12 17:25:37 +0200

    Lab3

---
+++ damneatrestaurant/src/main/java/com/damn/polito/damneatrestaurant/FirebaseTest.java
@@ -0,0 +1,44 @@
+package com.damn.polito.damneatrestaurant;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.damn.polito.damneatrestaurant.beans.DayOfTheWeek;
+import com.damn.polito.damneatrestaurant.beans.Dish;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class FirebaseTest extends AppCompatActivity {
+private String mail = "ste@gelato.it";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase_test);
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        Bitmap bmp = BitmapFactory.decodeResource(getResources(),R.drawable.dishes_empty);
+        Dish dish = new Dish("sss", "ddddd", 66, 66)
+        myRef.setValue(dish);
+//        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+//        DatabaseReference db_restaurant = database.child("ristoranti").child(mail);
+//
+//        init_firebase(database, db_restaurant);
+//
+//        DatabaseReference  db_dishes = db_restaurant.child("piatti");
+//        DatabaseReference  db_dishes_otd = db_restaurant.child("piatti_del_giorno");
+//        DatabaseReference  db_info = db_restaurant.child("profile_info");
+
+    }
+//
+//    private void init_firebase(DatabaseReference database, DatabaseReference db_restaurant){
+//        database.child("ristoranti").setValue(mail);
+//        db_restaurant.setValue("piatti");
+//        db_restaurant.setValue("piatti_del_giorno");
+//        db_restaurant.setValue("profile_info");
+//
+//    }
+}
