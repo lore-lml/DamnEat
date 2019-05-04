214d3e1b06a0ecab7e5527f5e929a8f6f710d36f
Stefano Loscalzo <stefano.loscalzo@gmail.com>
2019-05-04 02:24:24 +0200

    Accesso Firebase app deliverer

--- damneatdeliver/src/main/java/com/damn/polito/damneatdeliver/Welcome.java
+++ damneatdeliver/src/main/java/com/damn/polito/damneatdeliver/Welcome.java
@@ -1,16 +1,30 @@
 package com.damn.polito.damneatdeliver; //#7EE04A
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.damn.polito.damneatdeliver.R;
 import com.damn.polito.damneatdeliver.fragments.OrderFragment;
 import com.damn.polito.damneatdeliver.fragments.ProfileFragment;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Welcome extends AppCompatActivity {
+    List<AuthUI.IdpConfig> providers;
+    private final int REQUEST_CODE = 707;
 
     private FragmentManager fragmentManager;
     private ProfileFragment profileFragment;
@@ -43,6 +57,11 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+        );
+        shownSignInOptions();
 
         navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(navListener);
@@ -63,4 +82,39 @@ public class Welcome extends AppCompatActivity {
         if(selectedId != 0)
             navigation.setSelectedItemId(selectedId);
     }
+    private void shownSignInOptions() {
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setTheme(R.style.Background)
+                        .build(), REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE){
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if(resultCode==RESULT_OK){
+                //get user
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                //show email on toast
+                Toast.makeText(this, user.getEmail().toString(), Toast.LENGTH_LONG).show();
+                //set button signout
+                //b.setEnabled(true);
+                storeData(user);
+
+            }
+            else{
+                Toast.makeText(this, response.getError().getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void storeData(FirebaseUser user){
+        SharedPreferences.Editor pref = PreferenceManager.getDefaultSharedPreferences(this).edit();
+        pref.putString("dbkey", user.getUid());
+        pref.apply();
+    }
+
 }
