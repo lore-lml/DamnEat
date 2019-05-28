f3f74f2f5c01c90d3a77e21aa6195ed5db31130e
gigi <lpgpirisi@gmail.com>
2019-05-28 02:45:41 +0200

    Take NET position if the GPS has no signal (issue 33)

--- damneatdeliver/src/main/java/com/damn/polito/damneatdeliver/Welcome.java
+++ damneatdeliver/src/main/java/com/damn/polito/damneatdeliver/Welcome.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
@@ -40,6 +41,7 @@ import java.util.List;
 public class Welcome extends AppCompatActivity {
     //STATIC VARIABLES
     private static boolean logged;
+    private static final int ONE_MINUTE = 1000 * 60 ;
     private static Profile profile;
     private static Order currentOrder;
     private static String dbKey;
@@ -72,6 +74,7 @@ public class Welcome extends AppCompatActivity {
     private boolean mLocGranted;
     private LocationManager locationManager;
     private LocationListener locationListener;
+    private Location bestLocation;
 
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener
@@ -356,10 +359,14 @@ public class Welcome extends AppCompatActivity {
                 // @TODO: todo test
 //                String msg = "New Latitude: " + latitude + "New Longitude: " + longitude;
 //                Toast.makeText(Welcome.this, msg, Toast.LENGTH_LONG).show();
-                DatabaseReference RefLat = database.getReference("deliverers/" + dbKey + "/info/latitude");
-                RefLat.setValue(location.getLatitude());
-                DatabaseReference RefLong = database.getReference("deliverers/" + dbKey + "/info/longitude");
-                RefLong.setValue(location.getLongitude());
+                if(isBetterLocation(location,bestLocation)){
+                    DatabaseReference RefLat = database.getReference("deliverers/" + dbKey + "/info/latitude");
+                    RefLat.setValue(location.getLatitude());
+                    DatabaseReference RefLong = database.getReference("deliverers/" + dbKey + "/info/longitude");
+                    RefLong.setValue(location.getLongitude());
+                    bestLocation=location;
+                }
+
             }
 
             @Override
@@ -377,7 +384,16 @@ public class Welcome extends AppCompatActivity {
             }
         };
 
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 10, locationListener);
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Log.d("LOCATIONciao","gps");
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 800, 10, locationListener);
+        }
+        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            Log.d("LOCATIONciao","net");
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 800, 10, locationListener);
+        }
+
+        //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 10, locationListener);
     }
 
     private void getLocationPermissions() {
@@ -437,5 +453,63 @@ public class Welcome extends AppCompatActivity {
 
     public static String getDbKey() {
         return dbKey;
+    }
+
+
+
+    /** Determines whether one Location reading is better than the current Location fix
+     * @param location  The new Location that you want to evaluate
+     * @param currentBestLocation  The current Location fix, to which you want to compare the new one
+     */
+    protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+        if (currentBestLocation == null) {
+            // A new location is always better than no location
+            return true;
+        }
+
+        // Check whether the new location fix is newer or older
+        long timeDelta = location.getTime() - currentBestLocation.getTime();
+        boolean isSignificantlyNewer = timeDelta > ONE_MINUTE;
+        boolean isSignificantlyOlder = timeDelta < -ONE_MINUTE;
+        boolean isNewer = timeDelta > 0;
+
+        // If it's been more than two minutes since the current location, use the new location
+        // because the user has likely moved
+        if (isSignificantlyNewer) {
+            return true;
+            // If the new location is more than two minutes older, it must be worse
+        } else if (isSignificantlyOlder) {
+            return false;
+        }
+
+        // Check whether the new location fix is more or less accurate
+        int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+        boolean isLessAccurate = accuracyDelta > 0;
+        boolean isMoreAccurate = accuracyDelta < 0;
+        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+        // Check if the old and new location are from the same provider
+        boolean isFromSameProvider = isSameProvider(location.getProvider(),
+                currentBestLocation.getProvider());
+
+        // Determine location quality using a combination of timeliness and accuracy
+        if (isMoreAccurate) {
+            return true;
+        } else if (isNewer && !isLessAccurate) {
+            return true;
+        } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+            return true;
+        }
+        return false;
+    }
+
+    /** Checks whether two providers are the same */
+    private boolean isSameProvider(String provider1, String provider2) {
+        if (provider1 == null) {
+            return provider2 == null;
+        }
+        return provider1.equals(provider2);
+
+
     }
 }
