0c7ae7c25f540aa6390f9020f8723c81e33cd12d
pI88Id <pi.edoardo@gmail.com>
2019-06-03 02:14:35 +0200

    AutoZoom added, little graphical changes

--- app/src/main/java/com/damn/polito/damneat/FollowDelivererActivity.java
+++ app/src/main/java/com/damn/polito/damneat/FollowDelivererActivity.java
@@ -19,6 +19,7 @@ import com.damn.polito.commonresources.beans.Deliverer;
 import com.damn.polito.damneat.adapters.CustomInfoMarkerAdapter;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -26,6 +27,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.Task;
 
@@ -43,6 +45,8 @@ public class FollowDelivererActivity extends AppCompatActivity implements OnMapR
     private GoogleMap mMap;
     private FusedLocationProviderClient mFLPC;
     private Deliverer deliverer;
+    private Location currLoc;
+    private MarkerOptions place1, place2;
 
 
     @Override
@@ -54,6 +58,11 @@ public class FollowDelivererActivity extends AppCompatActivity implements OnMapR
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
 //        deliverer = Welcome.getDeliverer();
+
+        place1 = new MarkerOptions().position(new LatLng(currLoc.getLatitude(), currLoc.getLongitude()))
+                .title(deliverer.getName());
+        place2 = new MarkerOptions().position(new LatLng(deliverer.getLongitude(), deliverer.getLongitude()))
+                .title(deliverer.getName());
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -75,13 +84,12 @@ public class FollowDelivererActivity extends AppCompatActivity implements OnMapR
                 Task location = mFLPC.getLastLocation();
                 location.addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
-                        Location currLoc = (Location) task.getResult();
+                        currLoc = (Location) task.getResult();
 
-                        MarkerOptions marker = new MarkerOptions().position(new LatLng(currLoc.getLatitude(), currLoc.getLongitude())).title("Hello Maps");
 //                        MarkerOptions marker = new MarkerOptions().position(new LatLng(deliverer.getLatitude(), deliverer.getLongitude())).title(deliverer.getName())
 //                                .icon(getMarkerIconFromDrawable(getResources().getDrawable(R.drawable.ic_bike, null)))
 //                                ;
-                        mMap.addMarker(marker);
+//                        mMap.addMarker(marker);
                         moveCamera(new LatLng(deliverer.getLatitude(), deliverer.getLongitude()), DEFAULT_ZOOM, deliverer);
 
                         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(currLoc.getLatitude(), currLoc.getLongitude()), DEFAULT_ZOOM));
@@ -130,6 +138,31 @@ public class FollowDelivererActivity extends AppCompatActivity implements OnMapR
             mMap.setInfoWindowAdapter(new CustomInfoMarkerAdapter(FollowDelivererActivity.this, deliverer));
             return false;
         });
+
+        if(mMap!=null)
+            mMap.clear();
+        mMap = googleMap;
+        Drawable bike = getResources().getDrawable(R.drawable.bikerfinal, null);
+        BitmapDescriptor markerIcon = getMarkerIconFromDrawable(bike);
+
+        if(currLoc==null || place2==null)
+            return;
+        place2.icon(markerIcon);
+
+        mMap.addMarker(place1);
+        mMap.addMarker(place2);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(midPoint(place1,place2), 13));
+
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        builder.include(place1.getPosition());
+        builder.include(place2.getPosition());
+        LatLngBounds bounds = builder.build();
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, 100);
+        mMap.animateCamera(cameraUpdate);
+    }
+
+    public LatLng midPoint(MarkerOptions m1,MarkerOptions m2){
+        return new LatLng((m1.getPosition().latitude+m2.getPosition().latitude)/2,(m1.getPosition().longitude+m2.getPosition().longitude)/2);
     }
 
     private void getLocationPermissions() {
